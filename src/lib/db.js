326c31d2a71a0197766dd3149079e0c3904fce4d
Feba Thankam Moni <febathankammoni@gmail.com>
2025-09-01 13:05:31 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,23 +1,19 @@
 import mysql from 'mysql2/promise';
 
 export async function query({ query, values = [] }) {
-  // Connect to the database using the credentials from your .env.local file
   const dbconnection = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,  // 👈 Important for Railway
   });
 
   try {
-    // Run the SQL query
     const [results] = await dbconnection.execute(query, values);
-    // Close the connection
     dbconnection.end();
-    // Return the results
     return results;
   } catch (error) {
-    // If there's an error, log it and throw an error
     throw Error(error.message);
   }
 }
