c5f43d7862e525d47e6827234b34a2a88e746bce
Feba Thankam Moni <febathankammoni@gmail.com>
2025-09-01 22:31:39 +0530

    Update route.js

--- src/app/api/schools/route.js
+++ src/app/api/schools/route.js
@@ -1,3 +1,8 @@
+console.log("Vercel Cloudinary ENV:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 import { NextResponse } from "next/server";
 import { query } from "@/lib/db";
 import { v2 as cloudinary } from "cloudinary";
